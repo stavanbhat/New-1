2f3228774b5217f06e217e5313f04fecb6b589d7
stavanbhat <stavanbhatt1318@gmail.com>
2025-08-21 19:15:04 +0530

    It's an order

---
+++ 25BB5_045.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+    int a,b,result ;
+    clrscr();
+    printf("Enter first number");
+    scanf("%d,& a");
+    printf("Enter secondnumber");
+    scanf("%d,& b");
+    result=a+b;
+    getch();
+}
