cf528c70db2efd5e677e81687982eee9eff6984c
stavanbhat <stavanbhatt1318@gmail.com>
2025-08-22 07:39:00 +0530

    It's an order

---
+++ 25BBT_045.cpp(5).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int hours,minutes;
+clrscr;
+printf("Enter hours");
+scanf("%d,&hours");
+minutes = hours*60;
+printf("%d hours=%d minutes\n*");
+getch();
+}
