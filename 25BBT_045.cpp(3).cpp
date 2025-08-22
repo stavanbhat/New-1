187a634b6f4d13c3b0b774705a588c2999ab0375
stavanbhat <stavanbhatt1318@gmail.com>
2025-08-22 07:28:55 +0530

    It's an order

---
+++ 25BBT_045.cpp(3).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    int a,b,result;
+    clrscr ();
+    printf("Enter a");
+    scanf("%d,&b");
+    printf("Enter a");
+    scanf("%d,&b");
+    result = a*b;
+    getch ();
+}
